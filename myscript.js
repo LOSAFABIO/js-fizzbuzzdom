794967feab6b1b10baa81d31285843f0f28874e6
LOSAFABIO <fabio.losa95@gmail.com>
2021-12-10 16:42:23 +0100

    scritto html e css e JS, mancano i multipli

--- myscript.js
+++ myscript.js
@@ -1,9 +1,25 @@
 const container = document.getElementById( "container" );   // definiamo container
+const titolo = document.querySelector( ".titolo" ).innerHTML = "FizzBuzzDOM"; // definiamo titolo
+
 
 for ( let i = 1; i <= 100; i++ ){   // variabile cicli
     console.log(i);
+    
+   // let box = `<div class="box">` + i + `</div>`;   //variabile Box
+   // container.innerHTML += box; //far comparire box in container
 
-    let box = `<div class="box">` + i + `</div>`;   //variabile Box
 
-    container.innerHTML += box; //far comparire box in container
+        if (( i%3 ) && (i!=0)){
+            let box = `<div class="box sea">` + i + `</div>`;   //variabile Box multipo di 3
+        } else if (( i%5 ) && (i!=0)){
+            let box = `<div class="box sun">` + i + `</div>`;   //variabile Box multiplo di 5
+        } else if(( i%3) && (i!=0) && (i%5)) {
+            let box = `<div class="box red">` + i + `</div>`;   //variabile Box multiplo di 3 e 5
+        } else {
+            let box = `<div class="box">` + i + `</div>`;   //variabile Box
+            container.innerHTML += box; //far comparire box in container
+
+        }
+
+
 }
