398691de782abf7db4de9966bc3e96ec2abc6d97
LOSAFABIO <fabio.losa95@gmail.com>
2021-12-12 23:17:28 +0100

    Finitoo (:

--- myscript.js
+++ myscript.js
@@ -5,19 +5,22 @@ for ( let i = 1; i <= 100; i++ ){   // variabile cicli
     console.log(i);
 
     i != 0;
-//let box = `<div class="box">` + i + `</div>`;   //variabile Box
-//container.innerHTML += box; //far comparire box in container    
 
     if  (( i%3 == 0 ) && (i%5 == 0 )) { //divisibile per 3
-        let box = `<div class="box red">` + i + `</div>`;   //variabile Box multiplo di 3 e 5
-    } else if ( i%5 == 0 ) {
-        let box = `<div class="box sun">` + i + `</div>`;   //variabile Box multiplo di 5
-    } else if ( i%3 == 0 ) {
-        let box = `<div class="box sea">` + i + `</div>`;   //variabile Box multipo di 3
-    } else {
-        let box = `<div class="box">` + i + `</div>`;   //variabile Box
-        container.innerHTML += box; //far comparire box in container    
-    }
+    
+        container.innerHTML += `<div class="box red">` + "FizzBuzz" + `</div>`; //far comparire box in container    
 
+    } else if ( i%5 == 0 ) {
+        
+        container.innerHTML += `<div class="box sun">` + "Buzz" + `</div>`; //far comparire box in container    
+
+    } else if ( i%3 == 0 ) {
+        
+            container.innerHTML += `<div class="box sea">` + "Fizz" + `</div>`; //far comparire box in container    
+
+    } else {
+
+        container.innerHTML += `<div class="box">` + i + `</div>`; //far comparire box in container    
+        }
 
 }
