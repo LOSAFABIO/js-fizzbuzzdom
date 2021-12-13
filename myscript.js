4a40f93ce68b732ef27f755b52ded459a647e408
LOSAFABIO <fabio.losa95@gmail.com>
2021-12-13 09:56:52 +0100

    Corretto tutto

--- myscript.js
+++ myscript.js
@@ -4,21 +4,21 @@ const titolo = document.querySelector( ".titolo" ).innerHTML = "FizzBuzzDOM"; //
 for ( let i = 1; i <= 100; i++ ){   // variabile cicli
     console.log(i);
 
-    if  (( i%3 == 0 ) && (i%5 == 0 )) { //divisibile per 3
+    if  (( i%3 == 0 ) && (i%5 == 0 )) { //divisibile per 3 e 5
     
-        container.innerHTML += `<div class="box red">` + "FizzBuzz" + `</div>`; //far comparire box in container    
+        container.innerHTML += `<div class="box red"> FizzBuzz </div>`; //far comparire box Rosso in container    
 
-    } else if ( i%5 == 0 ) {
+    } else if ( i%5 == 0 ) { //divisibile per 5
         
-        container.innerHTML += `<div class="box sun">` + "Buzz" + `</div>`; //far comparire box in container    
+        container.innerHTML += `<div class="box sun"> Buzz </div>`; //far comparire box Giallo in container    
 
-    } else if ( i%3 == 0 ) {
+    } else if ( i%3 == 0 ) { //divisibile per 3 
         
-        container.innerHTML += `<div class="box sea">` + "Fizz" + `</div>`; //far comparire box in container    
+        container.innerHTML += `<div class="box sea"> Fizz </div>`; //far comparire box Celeste in container    
 
-    } else {
+    } else { //altrimenti numero
 
-        container.innerHTML += `<div class="box">` + i + `</div>`; //far comparire box in container    
+        container.innerHTML += `<div class="box"> + i + </div>`; //far comparire box Azzurro in container    
         
     }
 
